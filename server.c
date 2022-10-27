73ded08ec4836f3a49eada764d76aa4fd50384ff
aumarin <aumarin@student.42.fr>
2022-10-27 04:06:42 +0200

    prepush

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: aumarin <aumarin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 17:36:48 by aumarin           #+#    #+#             */
-/*   Updated: 2022/10/27 03:21:38 by aumarin          ###   ########.fr       */
+/*   Updated: 2022/10/27 04:06:23 by aumarin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,53 +35,58 @@ int	decode_save(int count, int sig)
 	return (char_dec_value);
 }
 
+int	init_message_length(int sig, int i, int len)
+{
+	if (sig == SIGUSR1)
+		len += 1 << i;
+	else if (sig == SIGUSR2)
+		len += 0 << i;
+	if (i == 0)
+	{
+		g_message = malloc(sizeof(char) * (len + 1));
+		if (!g_message)
+			return (-1);
+	}
+	i--;
+	return (len);
+}
+
+void	add_char_in_message(int *count, int *char_dec_value, int *e, int sig)
+{
+	if (--*count >= 0)
+	{
+		if (sig == SIGUSR1)
+			*char_dec_value += decode_save(*count, sig);
+	}
+	else
+	{
+		if (*char_dec_value != 0)
+			g_message[++*e] = *char_dec_value;
+		*count = 7;
+		*char_dec_value = 0;
+	}
+}
+
 void	handle_sig(int sig, siginfo_t *info, void *ucontext)
 {
 	static int	count = 8;
 	static int	char_dec_value = 0;
 	static int	len = 0;
-	static int	i = 31;
-	static int	e = 0;
+	static int	i = 32;
+	static int	e = -1;
 
 	(void)ucontext;
-	if (i >= 0)
-	{
-		if (sig == SIGUSR1)
-			len += 1 << i;
-		else if (sig == SIGUSR2)
-			len += 0 << i;
-		if (i == 0)
-		{
-			g_message = malloc(sizeof(char) * (len + 1));
-			if (!g_message)
-				return ;
-		}
-		i--;
-	}
+	if (--i >= 0)
+		len = init_message_length(sig, i, len);
 	else
 	{
-		count--;
-		if (count >= 0)
-		{
-			if (sig == SIGUSR1)
-				char_dec_value += decode_save(count, sig);
-		}
-		else
-		{
-			if (char_dec_value != 0)
-			{
-				g_message[e] = char_dec_value;
-				e++;
-			}
-			count = 7;
-			char_dec_value = 0;
-		}
+		add_char_in_message(&count, &char_dec_value, &e, sig);
 		if (count == 0 && char_dec_value == 0)
 		{
 			g_message[len] = '\0';
 			len = 0;
-			e = 0;
-			i = 31;
+			e = -1;
+			i = 32;
 			ft_printf("New message -> %s\n", g_message);
 			free(g_message);
 		}
@@ -106,7 +111,3 @@ int	main(void)
 		;
 	return (0);
 }
-
-/*
-! Taille du message a encoder sur 4 octets. 
-*/
